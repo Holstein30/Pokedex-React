344f03e762d3820f275b2a2961734ea687f43ac6
Zarnerz <holstein30@live.com>
2019-02-26 16:01:55 -0500

    fully functioning - need to resize/style a few assets - all working

--- src/components/DetailView.js
+++ src/components/DetailView.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./styles/DetailView.css";
 
-const DetailView = () => {
+const DetailView = ({ pokemon }) => {
+  const { id, name, sprite, type } = pokemon;
+
   return (
     <section className="detail-view">
-      <img alt="sprite" className="sprite-image" />
+      <img src={sprite} alt="sprite" className="sprite-image" />
       <div className="data-wrapper">
-        <h1 className="data-name" />
-        <p className="data-char" />
+        <h1 className="data-name">
+          Id: {id} {name}
+        </h1>
+        <p className="data-char">Type: {type}</p>
       </div>
     </section>
   );
